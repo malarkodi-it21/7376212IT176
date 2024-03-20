b17a0580ad3452557f4e96786f04f54f170406c1
malarkodi-it21 <95203223+malarkodi-it21@users.noreply.github.com>
2024-03-20 11:57:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import About from './Components/About';
+
+function App() {
+  return (
+    <div>
+   <About/>
+   </div>
+  );
+}
+
+export default App;
